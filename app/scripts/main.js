0d07f3728d70f26c2716e6c1388603322746151c
LuXiangrong <luxiangrongwave@gmail.com>
2015-05-27 22:08:17 +0800

    将代码移动到公共文件

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,35 @@
-document.body.addEventListener('touchstart', function () {});  
+'use strict';
+(function($) {
+    $(document).ready(function() {
+        $('.nav-region > li > a').on('click', function(e) {
+            e.preventDefault();
+            $(this).closest('.nav-region').find('> li').removeClass('active');
+            $(this).parent().addClass('active');
+
+            $(this).closest('.city-picker').find('.nav-city').html($(this).next().html());
+        });
+
+        $('.city-picker').on('click', '.nav-city > li > a', function(e) {
+            e.preventDefault();
+            $(this).closest('.nav-city').find('> li').removeClass('active');
+            $(this).parent().addClass('active');
+        });
+
+        $('[data-toggle="region"]').on('click', function(e) {
+            e.preventDefault();
+
+            var region = $(this).find('.region').text();
+            $(this).nextAll('.city-picker-wraper').toggle();
+            var cityPicker = $(this).nextAll('.city-picker-wraper').find('.city-picker');
+
+            cityPicker.find('.nav-region, .nav-city').find('li').removeClass('active');
+
+            cityPicker.find('[data-region="' + region + '"]').parent().addClass('active');
+
+            cityPicker.find('.nav-city').html(cityPicker.find('[data-region="' + region + '"]').next().html());
+
+            var city = $(this).next().find('.city').text();
+            cityPicker.find('.nav-city [data-city="' + city + '"]').parent().addClass('active');
+        });
+    });
+})(jQuery);
